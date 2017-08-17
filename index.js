d06c027a2e400150e3a36c6ed1207ff411199f66
Joel Guizar <joelguizar@gmail.com>
2017-08-16 21:02:37 -0700

    commands

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('lodash');
 
-let res = notes.add(1,2);
-console.log(res);
+let command = process.argv[2];
 
+if (command === "add") console.log('adding new note');
+if (command === "read") console.log('reading note');
+if (command === "list") {
+  console.log('listing all notes')
+} else {
+  console.log('command not recognized');
+}
 // let user = os.userInfo();
 //
 // fs.appendFile('greetings.txt', 'Hello' + user.username + 'you are ' + notes.age, err => err ? console.log(err) : console.log("success"))
