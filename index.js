b59aaa77c944b8c3b74fbc22d290c44b85deadfa
Joel Guizar <joelguizar@gmail.com>
2017-08-16 21:33:26 -0700

    yargs package

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 const os = require('os');
-const notes = require('./notes.js');
+const yargs = require('yargs');
 const _ = require('lodash');
+const notes = require('./notes.js');
+
 
 let command = process.argv[2];
 
 if (command === "add") console.log('adding new note');
 if (command === "read") console.log('reading note');
+if (command === "remove") console.log('removing note');
 if (command === "list") {
   console.log('listing all notes')
 } else {
