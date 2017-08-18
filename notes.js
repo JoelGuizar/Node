9cb921f3ab3a61b754c774483940271f5bc7d9bb
Joel Guizar <joelguizar@gmail.com>
2017-08-17 23:58:28 -0700

    remove note function

--- notes.js
+++ notes.js
@@ -7,6 +7,12 @@ const getAll = () => {
   console.log('getting all notes');
 };
 
+const remove = () => {
+  console.log('removing notes');
+}
+
 module.exports = {
-  addNote, getAll
+  addNote,
+  getAll,
+  remove
 }
